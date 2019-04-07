b2f3f3fcde68c09216caed4a57cda097d9cd0b6f
JakeWendao <liujingjieyear@163.com>
2019-04-07 14:29:35 +0800

    增加 test.py
    增加第一个主文件，test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+# author:jakewendao
+
+from selenium import webdriver
+import unittest
+import sys
+
+class TestLogin(unittest.TestCase):
+
+    
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_b(self):
+        self.driv er.get("http://www.baidu.com")
+        title = self.driver.title
+        print title
+    def tearDown(self):
+        self.driver.close()
+    def test_a(self):
+        self.driver.get("http://www.baidu.com")
+        title = self.driver.title
+        print title*2
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
